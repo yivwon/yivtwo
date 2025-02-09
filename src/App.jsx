051125e8e912d4332582e832f73bd5232495f4ae
Ivy Cho <ivyy.cho@gmail.com>
2025-02-09 00:17:01 -0500

    final commit before blender work

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import Scene from "./Scene";
-import videoSrc from "/assets/videos/HighTeaScene.mp4"; // Ensure the video file is in the correct path
+// import videoSrc from "/assets/videos/HighTeaScene.mp4"; // Ensure the video file is in the correct path
 
 // Array of video sources
 const videoSources = [
   "/assets/videos/CafeScene.mp4",
   "/assets/videos/HighTeaScene.mp4",
+  "/assets/videos/SleepyScene.mp4",
+
 ];
 
 export default function App() {
@@ -13,13 +15,10 @@ export default function App() {
   const [videoSrc, setVideoSrc] = useState("");
 
   useEffect(() => {
-// <<<<<<< HEAD
-    // Randomly select a video source
+
     const randomVideo = videoSources[Math.floor(Math.random() * videoSources.length)];
     setVideoSrc(randomVideo);
 
-// =======
-// >>>>>>> 51faad201f45985b819b5745fad8ce8e717a61fd
     const handleKeyPress = () => setStarted(true);
     window.addEventListener("keydown", handleKeyPress);
     return () => window.removeEventListener("keydown", handleKeyPress);
@@ -51,17 +50,12 @@ export default function App() {
               display: "flex",
               justifyContent: "center",
               alignItems: "center",
-// <<<<<<< HEAD
-              backgroundColor: "rgba(255, 255, 255, 0)",
-// =======
-              backgroundColor: "rgba(0, 0, 0, 0.5)",
-// >>>>>>> 51faad201f45985b819b5745fad8ce8e717a61fd
+              backgroundColor: "rgba(0, 0, 0, 0)",
               color: "white",
               fontSize: "24px",
               fontWeight: "bold",
             }}
           >
-            Press any key to continue...
           </div>
         </div>
       ) : (
